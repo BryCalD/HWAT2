3315a0ed0dcf4fac93b44c33bfdbf32caa124187
BryCalD <b00141439@mytudublin.ie>
2024-10-28 08:43:45 +0000

    added more types of barcodes in the scanner

--- src/app/scanner/barcode/page.js
+++ src/app/scanner/barcode/page.js
@@ -1,31 +1,38 @@
-'use client'
-import React, { useState } from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
-import CustomAppBar from '../../components/ResponsiveAppBarScanner'; // Import the CustomAppBar component
-
-// Dynamically import the scanner to work well with Next.js (no SSR)
-const BarcodeScannerComponent = dynamic(() => import('react-qr-barcode-scanner'), { ssr: false });
+import { BrowserMultiFormatReader } from '@zxing/library';
+import CustomAppBar from '../../components/ResponsiveAppBarScanner';
 
 const BarcodePage = () => {
   const [data, setData] = useState('No result');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const codeReader = new BrowserMultiFormatReader();
+    codeReader
+      .decodeFromVideoDevice(null, 'video', (result, err) => {
+        if (result) {
+          setData(result.getText());
+        } else if (err) {
+          
+        }
+      })
+      .catch(err => setError(err.message));
+
+    // Cleanup function to stop video stream when component unmounts
+    return () => codeReader.reset();
+  }, []);
 
   return (
     <div>
-      {/* Add the custom AppBar at the top */}
       <CustomAppBar />
 
-      <div style={{ textAlign: 'center', marginTop: '100px' }}> {/* Adjust margin to accommodate AppBar height */}
+      <div style={{ textAlign: 'center', marginTop: '100px' }}>
         <h1>Barcode Scanner</h1>
-        <div style={{ display: 'inline-block' }}>
-          <BarcodeScannerComponent
-            onUpdate={(err, result) => {
-              if (result) setData(result.text);
-              else setData('No result');
-            }}
-            style={{ width: '300px', height: '300px' }}  // Set the size to be smaller
-          />
-        </div>
+        <video id="video" style={{ width: '400px', height: '400px' }} /> {/* Specify video ID */}
         <p>{data}</p>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </div>
     </div>
   );
